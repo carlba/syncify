0e1e7606652f6a8853293c4f407a5367fdf17ede
Carl Bäckström <genzorg@gmail.com>
2018-08-22 12:18:42 +0200

    Working tar based implementation

--- syncify/core.py
+++ syncify/core.py
@@ -1,19 +1,19 @@
 # coding=utf-8
 
-import tarfile
 import sys
-import click
-from click.testing import CliRunner
-import os, contextlib
-import pathlib2 as pathlib
-# noinspection PyUnresolvedReferences
-from sh import pkill
-import grp
-import errno
 import shutil
+import os
+import contextlib
+import pathlib2 as pathlib
+
+import click
+import psutil
+import grp
 import fnmatch
-from sh import rsync, ssh, git, ErrorReturnCode_128, tar
-from simplepush import send, send_encrypted
+from click.testing import CliRunner
+# noinspection PyUnresolvedReferences
+from sh import rsync, ssh, git, ErrorReturnCode_128, tar, pkill, hdiutil, open
+
 
 applications = {
     'transgui': {
@@ -21,18 +21,18 @@ applications = {
         'paths': {'config': {'darwin': '$HOME/.config/Transmission Remote GUI'}}
     },
     'pycharm': {
-        'description': 'PyCharm 2017.3',
-        'paths': {'config': {'darwin': '$HOME/Library/Preferences/PyCharm2017.3',
-                             'linux2': '$HOME/.PyCharm2017.3/config'},
-                  'plugins': {'darwin': '$HOME/Library/Application Support/PyCharm2017.3',
+        'description': 'PyCharm 2018.3',
+        'paths': {'config': {'darwin': '$HOME/Library/Preferences/PyCharm2018.2',
+                             'linux2': '$HOME/.PyCharm2018.2/config'},
+                  'plugins': {'darwin': '$HOME/Library/Application Support/PyCharm2018.2',
                               'linux2': None}}
     },
     'webstorm': {
-        'description': 'WebStorm 2017.3',
+        'description': 'WebStorm 2018.2',
         'url': 'https://www.jetbrains.com/help/webstorm/directories-used-by-webstorm-to-store-settings-caches-plugins-and-logs.html',
-        'paths': {'config': {'darwin': '$HOME/Library/Preferences/WebStorm2017.3',
-                             'linux': '$HOME/.WebStorm2017.3/config'},
-                  'plugins': {'darwin': '$HOME/Library/Application Support/WebStorm2017.3',
+        'paths': {'config': {'darwin': '$HOME/Library/Preferences/WebStorm2018.2',
+                             'linux': '$HOME/.WebStorm2018.2/config'},
+                  'plugins': {'darwin': '$HOME/Library/Application Support/WebStorm2018.2',
                               'linux2': None}}
     },
     'development': {
@@ -50,9 +50,14 @@ applications = {
         'description': 'iTerm2',
         'url': 'https://www.alfredapp.com/help/troubleshooting/preferences',
         'paths': {'config': {'darwin': '$HOME/Library/Preferences/com.googlecode.iterm2.plist'}}
+    },
+
+    'calibre': {
+        'description': 'Calibre',
+        'url': 'https://manual.calibre-ebook.com/faq.html',
+        'paths': {'config': {'darwin': '$HOME/Library/Preferences/calibre'},
+                  'library': {'darwin': '$HOME/Calibre Library'}}
     }
-
-
 }
 
 script_dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -60,6 +65,8 @@ script_dir_path = os.path.dirname(os.path.realpath(__file__))
 excludes = {'/media/Windows/Users/genzo/Dropbox/transfer', '.cache', 'VirtualBox VMs',
             'Downloads', '.vagrant.d', '.dropbox', 'venv', 'Videos', '*.pyc', "compile-cache",
             '*.tmp', '*.*~', 'nohup.out', 'system/caches', 'node_modules', 'Cache', 'cache'}
+
+tarfile_output_path = '/Volumes/chomsky/transfer/syncify.tar.gz'
 
 
 @contextlib.contextmanager
@@ -99,7 +106,7 @@ def process_output(line):
     click.echo(line, nl=False)
 
 
-def rsynch_to(src, dst):
+def rsync_to(src, dst):
     if os.path.isdir(src):
         src += '/'
     if os.path.isfile(src):
@@ -110,7 +117,7 @@ def rsynch_to(src, dst):
           _out=process_output)
 
 
-def find_plattform_path(path):
+def find_platform_path(path):
     if 'all' in path:
         return path['all']
     elif sys.platform not in path or not path[sys.platform]:
@@ -129,9 +136,14 @@ def cli(ctx, output_path):
 
 @cli.command()
 @click.argument('application_names', nargs=-1)
-@click.option('--compress', '-c', is_flag=True, help='Compress to tar archive')
+@click.option('--clear-cache', '-c', is_flag=True)
 @click.pass_context
-def store(ctx, application_names, compress):
+def store(ctx, application_names, clear_cache):
+
+    if clear_cache:
+        shutil.rmtree(ctx.obj['output_path'])
+        pathlib.Path(ctx.obj['output_path']).mkdir(parents=True, exist_ok=True)
+        click.echo('Cache is cleared!')
 
     if not application_names:
         application_names = applications.keys()
@@ -142,21 +154,24 @@ def store(ctx, application_names, compress):
         else:
             # pkill(application_name)
             for path_name, path in applications[application_name]['paths'].viewitems():
-                expanded_platform_path = expand_vars_user(find_plattform_path(path))
+                expanded_platform_path = expand_vars_user(find_platform_path(path))
                 dst_path = create_tar_path(ctx.obj['output_path'], application_name, path_name)
                 click.echo('Synchronizing {}:{}'.format(application_name, path_name), color='green')
-                rsynch_to(src=expanded_platform_path, dst=dst_path)
+                rsync_to(src=expanded_platform_path, dst=dst_path)
 
-    if compress:
-        with remember_cwd():
-            os.chdir(os.path.dirname(ctx.obj['output_path']))
-            tar('--use-compress-program', '/usr/local/bin/pigz', '-czf', 'syncify.tar.gz', 'syncify', _bg=True)
+    with remember_cwd():
+        if "pigz" in (p.name() for p in psutil.process_iter()):
+            raise click.ClickException('Compression already active try again later')
+        os.chdir(os.path.dirname(ctx.obj['output_path']))
+        tar('--use-compress-program', '/usr/local/bin/pigz', '-czf',
+            tarfile_output_path, 'syncify', _bg=True)
 
 
 @cli.command()
 @click.argument('application_names', nargs=-1)
 @click.pass_context
 def load(ctx, application_names):
+
     if not application_names:
         application_names = applications.keys()
 
@@ -166,9 +181,14 @@ def load(ctx, application_names):
         else:
             # pkill(application_name)
             for path_name, path in applications[application_name]['paths'].viewitems():
-                expanded_platform_path = expand_vars_user(find_plattform_path(path))
+                expanded_platform_path = expand_vars_user(find_platform_path(path))
                 dst_path = create_tar_path(ctx.obj['output_path'], application_name, path_name)
-                rsynch_to(src=dst_path, dst=expanded_platform_path)
+                rsync_to(src=dst_path, dst=expanded_platform_path)
+
+    with remember_cwd():
+        os.chdir(os.path.dirname(ctx.obj['output_path']))
+        tar('--use-compress-program', '/usr/local/bin/pigz', '-czf',
+            "tarfile_output_path", 'syncify', _bg=True)
 
 
 def test_cli_store():
@@ -185,7 +205,8 @@ def main():
 
     if 'DEBUG' in os.environ and os.environ['DEBUG'] == 1:
         runner = click.testing.CliRunner()
-        result = runner.invoke(cli, ['--output_path', '/Users/cada/.config/syncify', 'store'], catch_exceptions=False)
+        result = runner.invoke(cli, ['--output_path', '/Users/cada/.config/syncify', 'store'],
+                               catch_exceptions=False)
         print result
     else:
         cli(obj={})
