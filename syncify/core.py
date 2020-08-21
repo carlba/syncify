4f5a3a310ec6275e31fe2ac1e98d95dbb5cd2346
Carl Bäckström <genzorg@gmail.com>
2020-08-21 11:02:02 +0200

    Cleaned up code added tests

--- syncify/core.py
+++ syncify/core.py
@@ -22,24 +22,28 @@ headers = {
     "Pragma": "no-cache"
 }
 
-r = requests.get('https://raw.githubusercontent.com/carlba/syncify/master/applications.json',
-                 headers=headers)
-applications = r.json()
-
-r = requests.get('https://raw.githubusercontent.com/carlba/syncify/master/settings.json',
-                 headers=headers)
-settings = r.json()
-
 script_dir_path = os.path.dirname(os.path.realpath(__file__))
 
-excludes = {'/media/Windows/Users/genzo/Dropbox/transfer', '.cache', 'VirtualBox VMs',
-            'Downloads', '.vagrant.d', '.dropbox', 'venv', 'Videos', '*.pyc', "compile-cache",
-            '*.tmp', '*.*~', 'nohup.out', 'system/caches', 'node_modules', 'Cache', 'cache',
-            'facebook_data', '*.mp4', 'social-log/messages'}
-
-tarfile_output_path = settings['tarfile_output_path']
-
 logger = create_logger()
+
+
+def read_settings():
+    r = requests.get('https://raw.githubusercontent.com/carlba/syncify/master/settings.json',
+                     headers=headers)
+    return r.json()
+
+
+def read_applications():
+    return requests.get(
+        'https://raw.githubusercontent.com/carlba/syncify/master/applications.json',
+        headers=headers).json()
+
+
+settings = read_settings()
+tarfile_output_path = settings['tarfile_output_path']
+applications = read_applications()
+excludes = settings['excludes']
+
 
 @contextlib.contextmanager
 def remember_cwd():
@@ -55,7 +59,33 @@ def expand_vars_user(path):
 
 
 def create_tar_path(output_path, application_name, path_name):
-    return os.path.join(output_path, '{}_{}'.format(application_name, path_name))
+    return os.path.join(output_path, f'{application_name}_{path_name}')
+
+
+def clear_path(path: str):
+    with remember_cwd():
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+            pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+            click.echo(f'Recreated {path}')
+        else:
+            pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+            click.echo(f'Created {path}')
+
+
+def compress(path: str):
+    with remember_cwd():
+        if "pigz" in (p.name() for p in psutil.process_iter()):
+            raise click.ClickException('Compression already active try again later')
+        os.chdir(os.path.dirname(path))
+        tar('--use-compress-program', '/usr/local/bin/pigz', '-czf',
+            expand_vars_user(tarfile_output_path), 'syncify', _bg=True)
+
+
+def clear_cache(path: str):
+    shutil.rmtree(path, ignore_errors=False)
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+    click.echo('Cache is cleared!')
 
 
 def reset_tar_info(members):
@@ -78,18 +108,19 @@ def process_output(line):
     click.echo(line, nl=False)
 
 
-def format_for_rsync(path: pathlib.Path, filetype: str):
-    return str(path.resolve()) + '/' if filetype == 'folder' else str(path.resolve())
-
-
-def rsync_to(src: pathlib.Path, dst: pathlib.Path, filetype: str):
+def rsync_to(src: pathlib.Path, dst: pathlib.Path, filetype: str, dry_run: bool):
     if filetype == 'file':
         dst.parent.mkdir(parents=True, exist_ok=True)
 
+    src_string = str(src) + '/' if filetype == 'folder' else str(src)
+    dst_string = str(dst) if filetype == 'folder' else str(dst.parent) + '/'
+
+    click.secho(f'Syncing path from {src_string} to {dst_string}', fg='green')
+
     exclude_params = zip(len(excludes) * ['--exclude'], excludes)
-    rsync('-rlt', '--out-format=%i: %n%L', '--max-size=200m',
-          format_for_rsync(src, filetype), format_for_rsync(dst, filetype), delete=True, *exclude_params,
-          _out=process_output)
+    if not dry_run:
+        rsync('-rlt', '--max-size=200m', '--stats', src_string, dst_string, delete=True,
+              *exclude_params, _out=process_output)
 
 
 def find_platform_path(path):
@@ -99,6 +130,25 @@ def find_platform_path(path):
         return False
     else:
         return path['platforms'][sys.platform]
+
+
+def get_sync_paths(applications: [], expanded_output_path: str, application_names: [] = None):
+    if not application_names:
+        application_names = applications.keys()
+
+    for application_name in application_names:
+        if not applications.get(application_name):
+            raise click.UsageError(f'Application {application_name} is not defined')
+        else:
+            # pkill(application_name)
+            for path in applications[application_name]['paths']:
+                expanded_platform_path = pathlib.Path(expand_vars_user(find_platform_path(path)))
+                archive_path = pathlib.Path(create_tar_path(expanded_output_path,
+                                                            application_name, path['name']))
+
+                if path['type'] == 'file':
+                    archive_path = archive_path / expanded_platform_path.name
+                yield expanded_platform_path, archive_path, path, application_name
 
 
 @click.group()
@@ -116,91 +166,46 @@ def cli(ctx, output_path, dry_run):
 @click.option('--clear-cache', '-c', is_flag=True)
 @click.pass_context
 def store(ctx, application_names, clear_cache):
+    expanded_output_path = expand_vars_user(ctx.obj['output_path'])
 
     if clear_cache:
-        shutil.rmtree(ctx.obj['output_path'], ignore_errors=False)
-        pathlib.Path(ctx.obj['output_path']).mkdir(parents=True, exist_ok=True)
-        click.echo('Cache is cleared!')
+        clear_path(expanded_output_path)
 
-    if not application_names:
-        application_names = applications.keys()
+    for sync_path in get_sync_paths(applications, expanded_output_path, application_names):
+        local_path, archive_path, path, application_name = sync_path
+        click.secho(f'Syncing path {path["name"]} for application {application_name} ',
+                    fg='green')
+        rsync_to(local_path, archive_path, path['type'], ctx.obj['dry_run'])
 
-    for application_name in application_names:
-        if not applications.get(application_name):
-            raise click.UsageError('Application {} is not defined'.format(application_name))
-        else:
-            # pkill(application_name)
-            for path in applications[application_name]['paths']:
-                expanded_platform_path = pathlib.Path(expand_vars_user(find_platform_path(path)))
-                dst_path = pathlib.Path(create_tar_path(ctx.obj['output_path'],
-                                                        application_name, path['name']))
-                click.secho(f'Syncing path {path["name"]} for application {application_name} '
-                            f'from {dst_path.resolve()} to {expanded_platform_path.resolve()}',
-                            fg='green')
-                if not ctx.obj['dry_run']:
-                    rsync_to(src=expanded_platform_path, dst=dst_path, filetype=path['type'])
-
-    with remember_cwd():
-        if "pigz" in (p.name() for p in psutil.process_iter()):
-            raise click.ClickException('Compression already active try again later')
-        os.chdir(os.path.dirname(ctx.obj['output_path']))
-        tar('--use-compress-program', '/usr/local/bin/pigz', '-czf',
-            expand_vars_user(tarfile_output_path), 'syncify', _bg=True)
+    if not ctx.obj['dry_run']:
+        compress(expanded_output_path)
 
 
 @cli.command()
 @click.argument('application_names', nargs=-1)
 @click.pass_context
 def load(ctx, application_names):
+    expanded_output_path = expand_vars_user(ctx.obj['output_path'])
 
-    with remember_cwd():
-        if os.path.isdir(ctx.obj['output_path']):
-            shutil.rmtree(ctx.obj['output_path'])
-            pathlib.Path(ctx.obj['output_path']).mkdir(parents=True, exist_ok=True)
-            click.echo('Recreated {}'.format(ctx.obj['output_path']))
-        else:
-            pathlib.Path(ctx.obj['output_path']).mkdir(parents=True, exist_ok=True)
-            click.echo('Created {}'.format(ctx.obj['output_path']))
+    if not ctx.obj['dry_run']:
+        clear_path(expanded_output_path)
 
-        os.chdir(os.path.dirname(ctx.obj['output_path']))
-        print(tar('-xzf', expand_vars_user(tarfile_output_path)))
-
-    if not application_names:
-        application_names = applications.keys()
-
-    for application_name in application_names:
-        logger.info(f'Restoring application {application_name}')
-        if not applications.get(application_name):
-            raise click.UsageError('Application {} is not defined'.format(application_name))
-        else:
-            # pkill(application_name)
-            for path in applications[application_name]['paths']:
-                expanded_platform_path = pathlib.Path(expand_vars_user(find_platform_path(path)))
-                dst_path = pathlib.Path(create_tar_path(ctx.obj['output_path'],
-                                                        application_name, path['name']))
-                if path['type'] == 'file':
-                    dst_path = dst_path / expanded_platform_path.name
-
-                click.secho(f'Syncing path {path["name"]} for application {application_name} '
-                            f'from {dst_path.resolve()} to {expanded_platform_path.resolve()}',
-                            fg='green')
-
-                if not ctx.obj['dry_run']:
-                    rsync_to(src=dst_path, dst=expanded_platform_path, filetype=path['type'])
+    for sync_path in get_sync_paths(applications, expanded_output_path, application_names):
+        local_path, archive_path, path, application_name = sync_path
+        click.secho(f'Syncing path {path["name"]} for application {application_name} ',
+                    fg='green')
+        rsync_to(archive_path, local_path, path['type'], ctx.obj['dry_run'])
 
 
 def test_cli_store():
     pass
 
     runner = click.testing.CliRunner()
-    # result = runner.invoke(store, ['pycharm'], catch_exceptions=False)
-    # print(result)
     result = runner.invoke(load, ['pycharm'], catch_exceptions=False)
     print(result)
 
 
 def main():
-
     if 'DEBUG' in os.environ and os.environ['DEBUG'] == 1:
         runner = click.testing.CliRunner()
         result = runner.invoke(cli, ['--output_path', '/Users/cada/.config/syncify', 'store'],
---
+++ syncify/test_core.py
@@ -0,0 +1,301 @@
+import os
+
+import pathlib
+
+from click.testing import CliRunner
+from syncify.core import rsync_to, rsync, store, load
+
+applications = {
+  "transgui": {
+    "description": "Transmission Remote GUI",
+    "paths": [
+      {
+        "name": "config",
+        "type": "folder",
+        "platforms": {
+          "darwin": "$HOME/.config/Transmission Remote GUI"
+        }
+      }
+    ]
+  },
+  "pycharm": {
+    "description": "PyCharm2020.2",
+    "paths": [
+      {
+        "name": "config",
+        "type": "folder",
+        "platforms": {
+          "darwin": "$HOME/Library/Application Support/JetBrains/PyCharm2020.2",
+          "linux2": None
+        }
+      }
+    ]
+  },
+  "webstorm": {
+    "description": "WebStorm2020.2",
+    "url": "https://www.jetbrains.com/help/webstorm/directories-used-by-webstorm-to-store-settings-caches-plugins-and-logs.html",
+    "paths": [
+      {
+        "name": "config",
+        "type": "folder",
+        "platforms": {
+          "darwin": "$HOME/Library/Application Support/JetBrains/WebStorm2020.2",
+          "linux": None
+        }
+      }
+    ]
+  },
+  "development": {
+    "description": "My development stuff",
+    "url": "https://github.com/carlba",
+    "paths": [
+      {
+        "name": "personal",
+        "type": "folder",
+        "platforms": {
+          "all": "$HOME/development"
+        }
+      },
+      {
+        "name": "work",
+        "type": "folder",
+        "platforms": {
+          "all": "$HOME/bsdev"
+        }
+      }
+    ]
+  },
+  "iterm2": {
+    "description": "iTerm2",
+    "url": "https://https://www.iterm2.com/",
+    "paths": [
+      {
+        "name": "config",
+        "type": "file",
+        "platforms": {
+          "darwin": "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+        }
+      }
+    ]
+  },
+  "calibre": {
+    "description": "Calibre",
+    "url": "https://manual.calibre-ebook.com/faq.html",
+    "paths": [
+      {
+        "name": "config",
+        "type": "folder",
+        "platforms": {
+          "darwin": "$HOME/Library/Preferences/calibre"
+        }
+      },
+      {
+        "name": "library",
+        "type": "folder",
+        "platforms": {
+          "darwin": "$HOME/Calibre Library"
+        }
+      }
+    ]
+  },
+  "settings": {
+    "description": "MacOS Settings",
+    "url": "none",
+    "paths": [
+      {
+        "name": "config",
+        "type": "folder",
+        "platforms": {
+          "darwin": "$HOME/settings"
+        }
+      }
+    ]
+  },
+  "hammerspoon": {
+    "description": "Hammerspoon settings",
+    "url": "none",
+    "paths": [
+      {
+        "name": "config",
+        "type": "folder",
+        "platforms": {
+          "darwin": "$HOME/.hammerspoon"
+        }
+      }
+    ]
+  },
+  "vscode": {
+    "description": "VSCODE settings",
+    "url": "https://code.visualstudio.com/docs/getstarted/settings",
+    "paths": [
+      {
+        "name": "settings_file",
+        "type": "file",
+        "platforms": {
+          "darwin": "$HOME/Library/Application Support/Code/User/settings.json",
+          "linux2": "$HOME/.config/Code/User/settings.json"
+        }
+      },
+      {
+        "name": "workspace",
+        "type": "folder",
+        "platforms": {
+          "darwin": "$HOME/.vscode",
+          "linux2": "$HOME/.vscode"
+        }
+      }
+    ]
+  }
+}
+
+
+settings = {
+  "tarfile_output_path": "$HOME/gdrive/transfer/syncify.tar.gz"
+}
+
+
+def test_folder_is_synced_properly(mocker):
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    rsync_to(pathlib.Path('/users/cbackstrom/.config/syncify/development_personal'),
+             pathlib.Path('/users/cbackstrom/development'), 'folder', False)
+
+    args, kwargs = patched_rsync.call_args
+
+    assert args[3] == '/users/cbackstrom/.config/syncify/development_personal/'
+    assert args[4] == '/users/cbackstrom/development'
+
+
+def test_file_is_synced_properly(mocker):
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    rsync_to(pathlib.Path('/users/cbackstrom/.config/syncify/development_personal/settings.json'),
+             pathlib.Path('/users/cbackstrom/development/settings.json'), 'file', False)
+
+    args, kwargs = patched_rsync.call_args
+
+    assert args[3] == '/users/cbackstrom/.config/syncify/development_personal/settings.json'
+    assert args[4] == '/users/cbackstrom/development/'
+
+
+def test_file_is_synced_properly_other_way(mocker):
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    rsync_to(pathlib.Path('/users/cbackstrom/development/settings.json'),
+             pathlib.Path('/users/cbackstrom/.config/syncify/development_personal/settings.json'), 'file', False)
+
+    args, kwargs = patched_rsync.call_args
+
+    assert args[3] == '/users/cbackstrom/development/settings.json'
+    assert args[4] == '/users/cbackstrom/.config/syncify/development_personal/'
+
+
+def test_it_is_not_possible_to_store_non_defined_app(mocker):
+
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    mocker.patch('syncify.core.clear_path', return_value=None)
+    mocker.patch('syncify.core.read_applications', return_value=applications)
+
+    runner = CliRunner()
+
+    result = runner.invoke(load, ['NonExistingApp'],
+                           obj={'output_path': '$HOME/.config/syncify', 'dry_run': False})
+
+    assert f'Application NonExistingApp is not defined' in result.output
+    patched_rsync.assert_not_called()
+
+
+def test_it_is_not_possible_to_load_non_defined_app(mocker):
+
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    mocker.patch('syncify.core.read_applications', return_value=applications)
+    mocker.patch('syncify.core.clear_path', return_value=None)
+
+    runner = CliRunner()
+
+    result = runner.invoke(load, ['NonExistingApp'],
+                           obj={'output_path': '$HOME/.config/syncify', 'dry_run': False})
+
+    assert f'Application NonExistingApp is not defined' in result.output
+    patched_rsync.assert_not_called()
+
+
+def test_loading_folder(mocker):
+
+    application_name = 'pycharm'
+    application = applications[application_name]
+    application_path = application['paths'][0]
+
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    mocker.patch('syncify.core.read_applications', return_value=applications)
+    mocker.patch('syncify.core.clear_path', return_value=None)
+
+    runner = CliRunner()
+
+    result = runner.invoke(load, [application_name],
+                           obj={'output_path': '$HOME/.config/syncify', 'dry_run': False})
+
+    args, kwargs = patched_rsync.call_args
+
+    assert args[3] == os.path.expandvars(os.path.expanduser(f'$HOME/.config/syncify/{application_name}_{application_path["name"]}')) + '/'
+    assert args[4] == os.path.expandvars(os.path.expanduser(application_path['platforms']['darwin']))
+
+def test_loading_file(mocker):
+
+    application_name = 'iterm2'
+    application = applications[application_name]
+    application_path = application['paths'][0]
+
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    mocker.patch('syncify.core.read_applications', return_value=applications)
+    mocker.patch('syncify.core.clear_path', return_value=None)
+
+    runner = CliRunner()
+
+    result = runner.invoke(load, [application_name],
+                           obj={'output_path': '$HOME/.config/syncify', 'dry_run': False})
+
+    args, kwargs = patched_rsync.call_args
+
+    assert args[3] == os.path.expandvars(os.path.expanduser(f'$HOME/.config/syncify/{application_name}_{application_path["name"]}/com.googlecode.iterm2.plist'))
+    assert args[4] == os.path.expandvars(os.path.expanduser(os.path.dirname(application_path['platforms']['darwin']))) + '/'
+
+
+def test_storing_folder(mocker):
+
+    application_name = 'pycharm'
+    application = applications[application_name]
+    application_path = application['paths'][0]
+
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    mocker.patch('syncify.core.read_applications', return_value=applications)
+    mocker.patch('syncify.core.clear_path', return_value=None)
+
+    runner = CliRunner()
+
+    result = runner.invoke(store, [application_name],
+                           obj={'output_path': '$HOME/.config/syncify', 'dry_run': False})
+
+    args, kwargs = patched_rsync.call_args
+
+    assert args[3] == os.path.expandvars(os.path.expanduser(application_path['platforms']['darwin'])) + '/'
+    assert args[4] == os.path.expandvars(os.path.expanduser(f'$HOME/.config/syncify/{application_name}_{application_path["name"]}'))
+
+
+def test_storing_file(mocker):
+
+    application_name = 'iterm2'
+    application = applications[application_name]
+    application_path = application['paths'][0]
+
+    patched_rsync = mocker.patch('syncify.core.rsync', return_value=None)
+    mocker.patch('syncify.core.read_applications', return_value=applications)
+    mocker.patch('syncify.core.clear_path', return_value=None)
+
+    runner = CliRunner()
+
+    result = runner.invoke(store, [application_name],
+                           obj={'output_path': '$HOME/.config/syncify', 'dry_run': False})
+
+    args, kwargs = patched_rsync.call_args
+
+    assert args[3] == os.path.expandvars(os.path.expanduser(application_path['platforms']['darwin']))
+    assert args[4] == os.path.expandvars(os.path.expanduser(f'$HOME/.config/syncify/{application_name}_{application_path["name"]}')) + '/'
+
